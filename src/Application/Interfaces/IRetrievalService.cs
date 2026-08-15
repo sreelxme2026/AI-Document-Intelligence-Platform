cd4964530e78acabecda2813d52f552769db3b0a
rinoyyy <165938690+rinoyyy@users.noreply.github.com>
2026-08-15 13:43:08 +0530

    feat: define retrieval application contract (#6)

---
+++ src/Application/Interfaces/IRetrievalService.cs
@@ -0,0 +1,10 @@
+﻿using Application.DTOs;
+
+namespace Application.Interfaces;
+
+public interface IRetrievalService
+{
+    Task<RetrievalResult> RetrieveAsync(
+        RetrievalRequest request,
+        CancellationToken cancellationToken);
+}
