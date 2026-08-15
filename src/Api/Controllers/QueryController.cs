f48f966ac5038b8147b02388f0bcc85daa14d40b
rinoyyy <165938690+rinoyyy@users.noreply.github.com>
2026-08-15 15:44:45 +0530

    feat: add query API endpoint (#10)

---
+++ src/Api/Controllers/QueryController.cs
@@ -0,0 +1,31 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/v1/query")]
+[Authorize]
+public class QueryController : ControllerBase
+{
+    private readonly IRagService _ragService;
+
+    public QueryController(IRagService ragService)
+    {
+        _ragService = ragService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<RagResult>> Query(
+        [FromBody] RagRequest request,
+        CancellationToken cancellationToken)
+    {
+        var result = await _ragService.GenerateAnswerAsync(
+            request,
+            cancellationToken);
+
+        return Ok(result);
+    }
+}
